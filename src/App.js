4ee3c76647b81dec3d4af54b7646327599a6999a
SteaceP <steacy@live.ca>
2021-02-24 10:43:22 -0500

    Added CheckoutPage with the item component, updated cart to use reselect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import { auth, createUserProfileDocument } from "./firebase/firebase";
 
 import HomePage from "./Pages/HomePage/Homepage";
 import ShopPage from "./Pages/Shop/ShopPage";
 import Header from "./Components/Header/Header";
 import SignupPage from "./Pages/SignupPage/SignupPage";
+import CheckoutPage from "./Pages/Checkout/CheckoutPage";
+
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 import "./App.css";
 
@@ -44,6 +48,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -57,8 +62,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/Components/Directory/Directory.jsx
+++ src/Components/Directory/Directory.jsx
@@ -16,33 +16,33 @@ class Directory extends React.Component {
         title: "jackets",
         imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
         id: 2,
-        linkUrl: "",
+        linkUrl: "jackets",
       },
       {
         title: "sneakers",
         imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
         id: 3,
-        linkUrl: "",
+        linkUrl: "sneakers",
       },
       {
         title: "womens",
         imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
         size: "large",
         id: 4,
-        linkUrl: "",
+        linkUrl: "womens",
       },
       {
         title: "mens",
         imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
         size: "large",
         id: 5,
-        linkUrl: "",
+        linkUrl: "mens",
       },
     ],
   };
 
   render() {
-    //otherSectionProps pass all the props of the sections state without explicitly identifying them all.
+    //otherSectionProps pass all the props of the sections state without explicitly identifying all of them.
     return (
       <div className="directory-menu">
         {this.state.sections.map(({ id, ...otherSectionProps }) => (
