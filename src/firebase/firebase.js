cd0e2756ec616d09ffbd6fcf7e0a58c883e0cb3f
Steacy Paquette <3757251+SteaceP@users.noreply.github.com>
2021-09-16 11:18:13 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyDL5V874Ju9PTRWJ8hMuerVnbjYpe0KRwI",
-  authDomain: "react-firebase-blog-fa4c7.firebaseapp.com",
-  projectId: "react-firebase-blog-fa4c7",
-  storageBucket: "react-firebase-blog-fa4c7.appspot.com",
-  messagingSenderId: "487295841277",
-  appId: "1:487295841277:web:d6b946f2147453a8cd9f94",
-  measurementId: "G-RVLWYQBYL8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(config);
